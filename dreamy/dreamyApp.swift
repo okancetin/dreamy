fa712c79228414747a40a6f67237184b1ab8a7b8
okan <okan@nex.ai>
2025-12-13 11:02:14 +0100

    Initial Commit

---
+++ dreamy/dreamyApp.swift
@@ -0,0 +1,17 @@
+//
+//  dreamyApp.swift
+//  dreamy
+//
+//  Created by okan on 13.12.25.
+//
+
+import SwiftUI
+
+@main
+struct dreamyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dreamyTests/dreamyTests.swift
@@ -0,0 +1,17 @@
+//
+//  dreamyTests.swift
+//  dreamyTests
+//
+//  Created by okan on 13.12.25.
+//
+
+import Testing
+@testable import dreamy
+
+struct dreamyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
